f95fc0c1abb718a60ec78fcb185f914a8aa8f18e
John Cosio <mail@jcos.io>
2019-05-13 03:10:31 +0800

    Added CICD functionality
    - Added Bitbucket Pipelines setup
    - Added Laravel Envoy blade
    
    Signed-off-by: John Cosio <mail@jcos.io>

---
+++ Envoy.blade.php
@@ -0,0 +1,45 @@
+@servers(['web' => $user.'@'.$host,'localhost' => '127.0.0.1'])
+
+@setup
+// Sanity checks
+if (empty($host)) {
+    exit('ERROR: $host var empty or not defined');
+}
+
+if (empty($user)) {
+    exit('ERROR: $user var empty or not defined');
+}
+
+if (empty($php)) {
+    exit('ERROR: $php var empty or not defined');
+}
+
+if (empty($code_directory)) {
+    exit('ERROR: $code_directory var empty or not defined');
+}
+
+// Command or path to invoke PHP
+$php = empty($php) ? 'php' : $php;
+@endsetup
+
+@story('deployBranch')
+    pipeline_task
+    server_task
+@endstory
+
+@task('pipeline_task', ['on' => 'localhost'])
+    echo "* Running example command on the Bitbucket Pipeline server *"
+    hostname
+    ls -la
+    ls -la {{ $code_directory }}
+@endtask
+
+@task('server_task', ['on' => 'web'])
+    echo "* Running example command on the remote server *"
+    hostname
+    ls -la
+    cd {{ $code_directory }}
+    @if ($branch)
+        git pull origin {{ $branch }}
+    @endif
+@endtask
